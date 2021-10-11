30775d34f2340ca22fdce1f3ae835c78e2fcc84b
Ahsan Khan <ahsanjkhan7@gmail.com>
2021-10-11 02:19:27 -0400

    #39 added post route in python and post form in html

--- flask_server.py
+++ flask_server.py
@@ -1,13 +1,23 @@
 #https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
 import sys
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #login()
+    return render_template('login.html')
+
+@app.route('/', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username)
+        print(password)
+    #print("HELLO WORLD")
 
 if __name__=="__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
