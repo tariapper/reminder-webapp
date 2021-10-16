3a34788dcc38f97e5f952411f7beb669597afa99
Tariq <tariapper@users.noreply.github.com>
2021-10-16 17:26:57 -0400

    #43 added links for login/register

--- flask_server.py
+++ flask_server.py
@@ -31,6 +31,9 @@ def index2():
 def index():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 @app.route('/navbar_test')
 def navbar():
